f27e8b4925f87e590bd24a9463c23b3cd8d91324
Brandon Leung <brandon.mcleung@castco.com.hk>
2022-11-21 17:01:52 +0800

    initial commit

---
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+    }
+}
