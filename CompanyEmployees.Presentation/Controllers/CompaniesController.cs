aa82e438fd50f61091653ee942413a0462bd6a85
Brandon Leung <brandon.mcleung@castco.com.hk>
2022-11-23 10:53:44 +0800

    add validation functions

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -38,6 +38,9 @@ namespace CompanyEmployees.Presentation.Controllers
             if (company is null)
                 return BadRequest("CompanyForCreationDto object is null");
 
+            if(!ModelState.IsValid)
+                return UnprocessableEntity(company);
+
             var createdCompany = _service.CompanyService.CreateCompany(company);
 
             return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
@@ -71,6 +74,9 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (company is null)
                 return BadRequest("CompanyForUpdateDto object is null");
+
+            if(!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
 
             _service.CompanyService.UpdateCompany(id,company,trackChanges: true);
             return NoContent();
