69871e79965d647652e7d465317defbef15094f0
Brandon Leung <brandon.mcleung@castco.com.hk>
2022-11-23 15:40:12 +0800

    add filter function to employee

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -29,6 +29,9 @@ namespace Service
 
         public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
+            if (!employeeParameters.ValidAgeRange)
+                throw new MaxAgeRangeBadRequestException();
+
             await CheckIfCompanyExists(companyId, trackChanges);
 
             var employeeswithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
